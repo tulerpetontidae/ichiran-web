0d9bfdb8813a1a44f8eaa881e2eb2d6aea74405e
tulerpetontidae <artem@lomakin.com>
2023-07-19 16:32:55 +0200

    corrected docker compose recipe

--- docker/ichiran-scripts/entrypoint.sh
+++ docker/ichiran-scripts/entrypoint.sh
@@ -9,5 +9,8 @@ done
 echo "Postgres is ready, starting main container init."
 init-all;
 
+echo "Starting Node.js server..."
+node /app/index.js & # Start the Node.js server in the background
+
 echo "All set, awaiting commands."
 sleep infinity;
